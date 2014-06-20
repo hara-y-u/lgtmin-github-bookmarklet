cfbedd9cfd6d9552bda54826b8de761cd5e8636a
yukihiro hara <yukihr@gmail.com>
2014-06-20 18:09:19 +0900

    first experimental commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var util = require('util')
+, koa = require('koa')
+, common = require('koa-common')
+, router = require('koa-router')
+, Q = require('q')
+, GitHubApi = require("github")
+, app = koa()
+, github = new GitHubApi({
+  // required
+  version: '3.0.0',
+  // optional
+  debug: true,
+  protocol: 'https',
+  timeout: 5000
+})
+;
+
+app.use(common.logger());
+app.keys = ['FIXME'];
+app.use(common.session());
+app.use(router(app));
+
+app.get('/', function *(next) {
+  // github api example
+  var res = yield Q.denodeify(github.user.getFollowingFromUser)({
+    user: 'mikedeboer'
+  }).then(function(res) {
+    return res;
+  });
+
+  // session example
+  var n = this.session.views || 0;
+  this.session.views = ++n;
+
+  this.body = n + 'views\n\n' + util.inspect(res);
+});
+
+app.get('/login', function *(next) {
+  
+});
+
+app.listen(3000);
