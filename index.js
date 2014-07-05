5beb67a700af5bae5c0aa0553aa1ad8adb6001e2
yukihiro hara <yukihr@gmail.com>
2014-07-05 23:37:19 +0900

    implement basic functionality

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var util = require('util')
 , koa = require('koa')
 , common = require('koa-common')
 , router = require('koa-router')
+, views = require('co-views')
+, parse = require('co-body')
 , Q = require('q')
+, request = Q.denodeify(require('request'))
 , port = process.env.PORT || 3000
 , app = koa()
 , csrf = require('koa-csrf')
@@ -20,14 +23,24 @@ app.use(common.logger());
 app.keys = [key];
 app.use(common.session());
 csrf(app);
-app.use(csrf.middleware);
-// app.use(function* () {
-//   if (this.method === 'GET') {
-//     this.body = this.csrf
-//   } else if (this.method === 'POST') {
-//     this.status = 204
-//   }
-// });
+app.use(function *(next) {
+  var _render = views('views', { default: 'jade' });
+  this.render = function (view, locals) {
+    var body, html
+    ;
+
+    if (!locals) locals = {};
+
+    return function *() {
+      this.type = 'text/html';
+      body = yield _render(view, locals);
+      html = yield _render('layout', { body: body });
+      this.body = html;
+    }
+  }
+
+  yield next;
+});
 app.use(router(app));
 
 client = new Client(app);
@@ -36,24 +49,45 @@ app.get('/', function *(next) {
   this.body = 'hello';
 });
 
-// TODO
 app.get('/lgtm', function *(next) {
-});
-
-app.get('/lgtm/create', client.requireAuth(function *(next) {
-  var ret
-  , req = this.request
-  , user = req.query.user
-  , repo = req.query.repo
-  , number = req.query.number
-  , hash = req.query.hash
+  var NUM_LGTMS = 3
+  , ret
+  , lgtmReqs = []
+  , lgtms = []
+  , i
   ;
 
+  for(i = 0; i < NUM_LGTMS; i++) {
+    lgtmReqs.push(request({
+      json: true
+      , url: 'http://www.lgtm.in/g'
+    }));
+  }
+
+  ret = yield lgtmReqs;
+
+  ret.forEach(function(_ret) { lgtms.push(_ret[1]); });
+
+  this.session._csrf
+  yield this.render('index', {
+    lgtms: lgtms
+    , query: this.request.query
+    , csrfToken: this.csrf
+  });
+});
+
+app.post('/lgtm/create', client.requireAuth(function *(next) {
+  var ret
+  , lgtm = yield parse(this)
+  ;
+
+  this.assertCsrf(lgtm);
+
   ret = yield Q.denodeify(this.github.issues.createComment)({
-    user: user
-    , repo: repo
-    , number: number
-    , body: lgtmMarkdown(hash)
+    user: lgtm.user
+    , repo: lgtm.repo
+    , number: lgtm.number
+    , body: lgtmMarkdown(lgtm.hash)
   }).then(function(comments) {
     return comments;
   });
