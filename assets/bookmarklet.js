94266ceaa18360c271bc3a8a8689d48a80176103
yukihiro hara <yukihr@gmail.com>
2014-07-13 15:21:06 +0900

    update bookmarklet

--- assets/bookmarklet.js
+++ assets/bookmarklet.js
@@ -4,20 +4,27 @@
   var nameRe = '([\-_\.A-z0-9]+)'
   , githubIssueUrlRe = new RegExp('https?://github.com/' + nameRe + '/'
                                   + nameRe + '/(?:pull|issues)/([0-9]+)')
-  , lgtmUrlTmp = 'http://lgtmin-gh-bmlt.herokuapp.com/lgtm?user=$USER&repo=$REPO&number=$NUMBER'
+  , homeUrl = 'http://lgtmin-gh-bmlt.herokuapp.com'
+  , lgtmUrlTemplate = homeUrl + '/lgtm?user=$USER&repo=$REPO&number=$NUMBER'
   , match, user, repo, number
   ;
 
   match = location.href.match(githubIssueUrlRe);
 
   if(!match || !match[1] || !match[2] || !match[3]) {
-    window.alert('Error!: Execute this bookmarklet on github issue page.');
+    if(location.href.indexOf(homeUrl) == 0) {
+      window.alert('Bookmark Me!');
+    } else {
+      window.alert('Error!: Execute this bookmarklet on github issue page.');
+    }
     return;
   }
 
   user = match[1], repo = match[2], number = match[3];
 
-  window.open(lgtmUrlTmp.replace('$USER', user)
+  window.open(lgtmUrlTemplate
+              .replace('$USER', user)
               .replace('$REPO', repo)
-              .replace('$NUMBER', number));
+              .replace('$NUMBER', number)
+             ).focus();
 })(window);
